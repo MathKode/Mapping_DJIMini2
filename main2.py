6cdac312a75f5bde91c28dbb7628dbb84977c63d
BiMathAx <61063210+MathKode@users.noreply.github.com>
2023-05-10 12:28:15 +0200

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,5 +1,5 @@
 # Mapping for DJI Drone
-# V0.1 by Bimathax
+# V1.4 by Bimathax
 import PIL.Image
 from PIL.ExifTags import TAGS, GPSTAGS
 from os import listdir, path
@@ -10,18 +10,22 @@ from matplotlib import pyplot
 
 ####### CONSTANT #######
 picture_folder = "pictures"
-result_name = "final____.jpeg" #-0.175 rad or -10.0267 deg
-meter_by_pixel_nb = 1
-""" Soit ALTITUDE OR PICTURE OR COTA (not the three) """
+result_name = "final_______.jpeg"
+"""----------------"""
 altitude = None #m
 picture_x_meter = None #m
 picture_y_meter = None #m
-cota_x = 4 # pixel/m
+cota_x = 8 # pixel/m
 cota_y = 3 # pixel/m
+"""----------------"""
 Blur = False #True Or False
 show_graph = False
+verbose = 3 #LEVEL 0-1-2-3 (Nothing,Error and Major Info, Normal, All)
+setting_file = True
+"""----------------"""
 picture_align_type = 1 #1=Horizontal ; 2=Vertical
 picture_align = ["reduce_DJI_0205.JPG","reduce_DJI_0213.JPG"] 
+angle = 24 #deg
 ########################
 
 def get_exif(pic_folder,picture_name):
@@ -31,10 +35,10 @@ def get_exif(pic_folder,picture_name):
         tag for tag, name in TAGS.items() if name == "GPSInfo"
     )
     gps_info = exif_data.get_ifd(GPSINFO_TAG)
-    return gps_info, img.width, img.height #DMS {0: b'\x02\x03\x00\x00', 1: 'N', 2: (44.0, 21.0, 40.8486), 3: 'E', 4: (5.0, 10.0, 1.465), 5: b'\x00', 6: 116.042}
+    return gps_info, img.width, img.height #DMS {0: b'\x02\x03\x00\x00', 1: 'N', 2: (44.0, 21.0, 8), 3: 'E', 4: (5.0, 10.0, 9), 5: b'\x00', 6: 116.042}
     
 def coordonate_to_decimal(degree,minute,second):
-    print(degree,minute,second)
+    #print(degree,minute,second)
     result = float(degree)+float(minute)/60+float(second)/3600
     return result
     
@@ -49,8 +53,8 @@ def haversine(lat1,long1,lat2,long2):
     lat2=dg_to_rad(lat2)
     long2=dg_to_rad(long2)
 
-    print(lat1,long1)
-    print(lat2,long2)
+    #print(lat1,long1)
+    #print(lat2,long2)
 
     # Rayon moyen de la terre 6_371_005.076123 metres
     #d en mètre
@@ -76,18 +80,17 @@ def calibration_newcenter(center_name, meter_pic):
 def angle_prediction(center_pic,picture_align_name,picture_align_type):
     if picture_align_type==1:
         x, y=center_pic[picture_align_name]
-        print(x,y)
         angle=atan(x/y)
         if x>=0 and y<=0:
             #Angle=-45 Ajoute 90 deg
-            print("Changement angle 1")
+            #print("Changement angle 1")
             angle=angle*-1+pi/2
         elif x<=0 and y<=0:
-            print("Changement angle 2")
+            #print("Changement angle 2")
             angle=angle*-1-pi/2
     else :
         x, y=center_pic[picture_align_name]
-        print(x,y)
+        #print(x,y)
         angle=(1/2*pi-atan(x/y))*-1
     return angle
     
@@ -132,15 +135,15 @@ def final_picture_size(rotate_pic,height_pic,width_pic, cota_x, cota_y):
     #print("HERE",rotate_pic)
     delta_x = x_max-x_min
     delta_y = y_max-y_min
-    print("Delta x:",delta_x,"\nDelta y:",delta_y)
+    #print("Delta x:",delta_x,"\nDelta y:",delta_y)
     width = delta_x*cota_x + width_pic
     height = delta_y*cota_y + height_pic
     
     #Bottom Left POINT COORDONATE IN METER
     x=x_min-(1/2*width_pic)/cota_x
     y=y_min-(1/2*height_pic)/cota_y
-    print("BOTTOM LEFT (x,y) :",x,y)
-    print(y, y_min)
+    #print("BOTTOM LEFT (x,y) :",x,y)
+    #print(y, y_min)
     
     return int(width+1), int(height+1), [x,y]
 
@@ -187,15 +190,32 @@ def copier_image(pic_folder,picture_name,begin_x,begin_y,image_result,blur):
                     image_result.putpixel((begin_x+x,begin_y+y),middle)
     return image_result
 
-def main(pic_folder,result_name,altitude,picture_x_meter,picture_y_meter,blur,show_graph,picture_align,picture_align_type,cota_x,cota_y):
+def setting_file_save(result_name,cota_x,cota_y,picture_x_meter,picture_y_meter,altitude):
+    file=open(f"SETTING_{result_name}.txt","w")
+    file.write(result_name + "\n")
+    file.write(f"Altitude : {altitude}")
+    file.write(f"Cota X : {cota_x}")
+    file.write(f"Cota Y : {cota_y}")
+    file.write(f"picture_x_meter : {picture_x_meter}")
+    file.write(f"picture_y_meter : {picture_y_meter}")
+
+def main(pic_folder,result_name,altitude,picture_x_meter,picture_y_meter,blur,show_graph,picture_align,picture_align_type,cota_x,cota_y,angle,verbose,setting_file):
     ls_pic=listdir(pic_folder)
-    print(ls_pic)
+    if verbose==3:
+        print("--- Liste Image ---")
+        print(ls_pic,"\n")
+    
     gps_pic={} #long, lat
     meter_pic={} #x, y
     pic_width=0;pic_height=0;altitude_sea=0;nb_pic=0
     for i in ls_pic:
-        print("GET EXIF:", i)
+        if verbose==3 or verbose==2:
+            print("\n--- Exif ---\n" + str(i))
         gps, pic_width, pic_height = get_exif(pic_folder,i)
+        if verbose==3:
+            print("GPS:",gps)
+            print("X-Y:",pic_width,pic_height)
+        
         altitude_sea+=gps[6]
         nb_pic+=1
 
@@ -207,124 +227,167 @@ def main(pic_folder,result_name,altitude,picture_x_meter,picture_y_meter,blur,sh
         lat=nb_lat*coordonate_to_decimal(gps[2][0],gps[2][1],gps[2][2])
         long=nb_long*coordonate_to_decimal(gps[4][0],gps[4][1],gps[4][2])
         gps_pic[i]=[long,lat]
-        print(lat,long)
+        if verbose == 3:
+            print("Latitude :",lat, "\nLongitude :",long)
         y, x = deg_to_meter(lat,long)
+        if verbose == 3 or verbose == 2:
+            print(f"--- Convertion in Meter ---\nLat : {y}\nLong : {x}")
         meter_pic[i]=[x,y]
     #Moyenne Altitude
     altitude_sea=int(altitude_sea/nb_pic)
-    print("ALTITUDE MER",altitude_sea)
-    print("ALTITUDE VOL",altitude)
-    print("TAILLE PHOTO px(X-Y)",pic_width,pic_height)
+    if verbose==3:
+        print("\n--- Altitude ---")
+        print("Sea :",altitude_sea)
+        print("Vol :",altitude)
+    if verbose>=1:
+        print("\n--- Size Picture ---")
+        print("Width pixel :",pic_width)
+        print("Height pixel :",pic_height)
 
-    print(gps_pic)
-    print(meter_pic)
+    if verbose>=3:
+        print("\n--- GPS Dictionnary ---")
+        print(gps_pic)
+        print("\n--- Meter Dictionnary ---")
+        print(meter_pic)
     
     if show_graph:
         graph_representation(meter_pic)
 
-    """
-    new_center_img_name=None
-    for i in meter_pic:
-        new_center_img_name=i
-        break
-    """
     for i in picture_align:
         if i not in meter_pic:
-            print("ERREUR Noms images pour alignements")
+            if verbose>=1:
+                print(">>> ERROR : Name from picture_align not find in the folder")
             exit(0)
     new_center_img_name=picture_align[0]
-    print("IMG Center",new_center_img_name)
+    if verbose>=2:
+        print(f"\n--- Picture Center ---\nCenter name : {new_center_img_name}")
     
+    #center_pic = {}
     center_pic=calibration_newcenter(new_center_img_name, meter_pic)
-    print(center_pic)
+    if verbose>=3:
+        print("\n--- Centered Pictures List ---")
+        print(center_pic)
 
     if show_graph:
         graph_representation(center_pic)
     
-    rotate_pic={}
-    angle=1.2992 #dg
-    angle=angle_prediction(center_pic, picture_align[1],picture_align_type)*180/pi
-    print('ANGLE:',angle)
-    print("Voulez vous gardez cette angle prédit [O/valeur] :")
-    ans=input("->").lower()
-    if ans != "o":
-        angle=float(ans)
-        print("NEW ANGLE :",angle)
-    angle=angle*pi/180
-    print("ANGLE in radian",angle)
+    if verbose >= 2:
+        print("\n--- Set Angle ---")
     
-    #angle=1.3029183314941166
+    rotate_pic={}
+    if angle==None :
+        angle=angle_prediction(center_pic, picture_align[1],picture_align_type)*180/pi
+    if verbose>=1:
+        print(f"\n--- Angle ---\nDegre : {angle}")
+    angle=angle*pi/180
+    if verbose>=1:
+        print("Radian :",angle)
+    
+    if verbose >= 2:
+        print("\n--- Rotate Picture New Coordonates ---")
     for i in center_pic:
         x, y=center_pic[i]
         new_x, new_y = rotation_center(angle,x,y)
         rotate_pic[i] = [new_x, new_y]
-    
-    print(rotate_pic)
+    if verbose>=3:
+        print(rotate_pic)
     if show_graph:
         graph_representation(rotate_pic)
     
 
-    #### MINI 2 SPEC ####
-    """ NO OPERATIONAL
-    focal=4.49 #mm
-    sensor_x=58.42 #mm
-    sensor_x=25.4
-    cota = cota_pronostic_byaltitude(focal,sensor_x,pic_width,altitude)
-    print(cota)
-    """
     
     #https://pixspy.com/
     #COTA X
+    if verbose>=1:
+        print("\n--- Cota X ---")
     if cota_x==None:
         if picture_x_meter==None:
-            print("FIND IMAGE X Meter BY ALTITUDE")
+            if verbose>=2:
+                print("By altitude")
             picture_x_meter=image_x_pronostic(altitude)
-            print("picture x meter",picture_x_meter)
-        cota_x = int(pic_width/picture_x_meter)
+            if verbose>=3:
+                print("Picture x meter",picture_x_meter)
+            cota_x = int(pic_width/picture_x_meter)
+        else:
+            if verbose>=2:
+                print("By Picture_x_meter")
+                cota_x = int(pic_width/picture_x_meter)
     else:
+        if verbose>=2:
+            print("By variable (already def)")
         cota_x=int(cota_x)
+    if verbose>=1:
+        print("Cota X :",cota_x)
     
     #COTA Y
+    if verbose>=1:
+        print("\n--- Cota Y ---")
     if cota_y==None:
         if picture_y_meter==None:
-            print("FIND IMAGE Y Meter BY ALTITUDE")
+            if verbose>=2:
+                print("By altitude")
             picture_y_meter=image_y_pronostic(altitude)
-            print("picture y meter",picture_y_meter)
-        cota_y = int(pic_height/picture_y_meter)
+            if verbose>=3:
+                print("picture y meter",picture_y_meter)
+            cota_y = int(pic_height/picture_y_meter)
+        else:
+            if verbose>=2:
+                print("By Picture_y_meter")
+            cota_y = int(pic_height/picture_y_meter)
     else:
+        if verbose>=2:
+            print("By variable (already def)")
         cota_y = int(cota_y)
-    print("COTA X-Y",cota_x,cota_y)
+    if verbose>=1:
+        print("Cota Y :",cota_y)
     
     
     width, height, bottom_left=final_picture_size(rotate_pic,pic_height,pic_width,cota_x,cota_y) #Cota px par m
-    print("FINAL SIZE X-Y BOTTOMLEFT",width, height, bottom_left)
+    if verbose>=2:
+        print(f"--- Final Size ---\nPixel X : {width}\nPixel Y : {height}\nBottom_Left_Corner in meter : {bottom_left}")
 
+    if verbose>=2:
+        print("\n--- Find Pixel Position ---")
+    
     beginpixel_pic = {}
     for i in rotate_pic:
         middle_x, middle_y = rotate_pic[i]
         beginpixel_pic[i] = find_beginning_top_left_pixel(middle_x,middle_y,cota_x,cota_y,bottom_left[0],bottom_left[1],pic_width,pic_height)
 
+    if verbose>=3:
+        print(beginpixel_pic)
     if show_graph:
         graph_representation(beginpixel_pic)
+
+    if verbose>=1:
+        print("\n--- Result Creation ---")
+    
     result = PIL.Image.new(mode="RGB", size=(width, height))
     for i in beginpixel_pic:
         try:
-            print("ADD",i)
+            if verbose>=1:
+                print("Add :",i)
             result = copier_image(pic_folder,i,beginpixel_pic[i][0],beginpixel_pic[i][1],result,blur)
-            print("END")
+            if verbose>=3:
+                print("Added !")
         except:
-            print("erreur img",i)
-            print(f"beginpx {beginpixel_pic[i]} size all (x-y) {width} {height} size litte {pic_width} {pic_height}")
+            if verbose>=1:
+                print(">>> ERROR : With the add of the image",i)
+            if verbose>=2:
+                print(f"beginpx {beginpixel_pic[i]} size all (x-y) {width} {height} size litte {pic_width} {pic_height}")
     #result.putpixel((30,30),(0,255,255))
+    ok=True
     try:
         result.save(result_name,"JPEG")
     except:
-        print("Erreur Save")
-    print("Save AS :",result_name)
+        ok=False
+        if verbose>=1:
+            print(">>> Error : Can't save the picture",result_name)
+    if verbose>=1 and ok:
+        print("Save AS :",result_name)
     result.show()
+    if ok and setting_file:
+        setting_file_save(result_name,cota_x,cota_y,picture_x_meter,picture_y_meter,altitude)
 
 
-
- 
-
-main(picture_folder,result_name,altitude, picture_x_meter,picture_y_meter,Blur,show_graph,picture_align,picture_align_type,cota_x,cota_y)
+main(picture_folder,result_name,altitude, picture_x_meter,picture_y_meter,Blur,show_graph,picture_align,picture_align_type,cota_x,cota_y,angle,verbose,setting_file)
